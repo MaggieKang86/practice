f9c4cb883f74ad6b2a4bc077fdbf13fdde5719ba
maggiekang86 <maggiekang86@gmail.com>
2023-10-19 16:37:42 +0800

    完成身份驗證level 1練習

--- HW/src/homework/verify/IdVerifyHelper.java
+++ HW/src/homework/verify/IdVerifyHelper.java
@@ -9,40 +9,20 @@ import java.util.ArrayList;
 public class IdVerifyHelper {
 
     public static void main(String[] args) {
-        // 好 現在來處理問題 如果第二行執行到會有甚麼問題?
-        // 到計算w2~w9的時候會錯誤 甚麼錯誤 這個方法是你自己想的嗎
-        // 對啊XD  我是把每個身分證字號拆開來看，先把每一個身分證字號放進String[]裡，
-        // 選 index1 得去看看字母換成數字變成多少
-        // 再抓 index 2~9 轉成 int 並放進 int[] 去算總和
-        // 最後再算出 index 10
-        
-        // 最後驗證
-        // 那為什麼 error404 不行 它是哪一個字母不行
-        // 因為他 index 2 是 r ，不能參與 w2~9 的計算
-        // 為甚麼 r 不行
-        // 我把 index 2~9 從 String 換成 int 去算 然後呢
-        // 所以 r 不是 int 就報錯惹  那為什麼 e 沒有抱錯
-        // 因為 index 1 的算法是用他去比對 A~Z 是=哪個位置 推出對應的數字
-        // 所以身分證應該是 第一個碼是英文 但第二行變成 error 你是這樣判斷的 後面幾個是數字 後面幾個不是數字所以判段失敗 這樣嗎
-        // 對
-        // 那你是不是可以再多加一個邏輯 
-        // 就是只要第二碼不是數字就打印驗證失敗嗎
-        // 對 你可以去看正則表達式，正則表達式就可以不用迴圈判斷字母 你丟一個字串進去看這個字串有沒有符合你的規則 沒有你就直接打印失敗了
-        // 所以你要在之前就把他擋掉，因為要符合你的第二個字母開始是數字才能執行你的方法
-        // 這樣可以嗎
-        // 懂！我也是這麼想，只是我不豬道要怎麼擋掉他 偶來去看正則表達式
-        // 寫完就line我跟我說你寫完了
-        // 豪
-        // 你上面提到的 只要第二碼不是數字就打印驗證失敗嗎 這邊會有一個問題 那我今天把 error404 改成 e2ror404 你這樣不就還要再繼續去判斷後面幾碼後面幾碼
-        // 一樣報錯
-        // 對 所以比較好就是用正則表達式 去組成
-        // 豪 我看一下說明
-        // https://hsueh-jen.gitbooks.io/webcrawler/content/zheng-gui-biao-shi-shi/zheng-gui-biao-shi-shi.html
-        // 上面這個網址看一下
-        // 好
-        ArrayList<String> lines = new ArrayList<>();
         String filePath = "HW/src/homework/verify/idList.txt";   // 檔案路徑
-        // String filePath = "/Users/maggie/Downloads/idList.txt";   // 檔案路徑
+        confirm(filePath);
+    }
+    // 那這一關就先到這邊 給你看看我寫的 你休息一下 明天再繼續第二關
+    // 好啊我順便去看一些其他小東西 TAT
+    // 我把我的程式碼貼到 IdverifyHelper2
+    // 好～～
+    // 我有寫兩版 一種是我一開始寫的 後面是我學到新特性 用在裡面的 我給你第一種就好了
+    // 好呀
+    // 好了 你push 你自己一版就好  2你不要push
+    // 那我push上去存起來哦！
+    public static void confirm(String filePath) {
+        ArrayList<String> lines = new ArrayList<>();
+
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = reader.readLine()) != null) {
@@ -53,7 +33,6 @@ public class IdVerifyHelper {
         }
 
         String[] fileContent = lines.toArray(new String[0]);  // 將 ArrayList 轉換為 String[] fileContent 數組
-
 
         for (int i = 0; i < fileContent.length; i++) {                 // 開始計算檔案內的每條數據
             String s = fileContent[i];                                 // 獲取 index i 的字串 s
@@ -133,14 +112,12 @@ public class IdVerifyHelper {
                     System.out.println("-----------------------\n");
                 }
 
-            }else {
+            } else {
                 System.out.println("==== 您輸入的身分證字號 " + s + " ====");
                 System.out.println("==== 驗證失敗 ====");
                 System.out.println("-----------------------\n");
             }
         }
-
-
     }
 }
 

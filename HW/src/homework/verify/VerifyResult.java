1daf514e458406434edcf1a0b8df8b8a74dabb8c
maggiekang86 <maggiekang86@gmail.com>
2023-10-23 15:46:15 +0800

    刪除不必要的import

---
+++ HW/src/homework/verify/VerifyResult.java
@@ -0,0 +1,51 @@
+package homework.verify;
+
+public class VerifyResult {
+
+    private Boolean isVerifySuccess;
+    private String id;
+    private String message;
+
+    public VerifyResult(Boolean isVerifySuccess, String id, String message) {
+        this.isVerifySuccess = isVerifySuccess;
+        this.id = id;
+        this.message = isVerifySuccess ? "驗證成功" : "驗證失敗";
+    }
+
+    public VerifyResult() {
+    }
+
+    @Override
+    public String toString() {
+        return
+                "isVerifySuccess = " + isVerifySuccess + "\n" +
+                        "id = " + id + "\n" +
+                        "message = " + message + "\n" +
+                        "-----------------------" + "\n";
+    }
+
+    public Boolean getVerifySuccess() {
+        return isVerifySuccess;
+    }
+
+    public void setVerifySuccess(Boolean verifySuccess) {
+        isVerifySuccess = verifySuccess;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
